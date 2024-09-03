234735d1c05897262bc04f9c3d483aa50c6ea549
MariamCHARRADA <charradamariam@yahoo.com>
2024-09-03 17:32:56 +0100

    Create React tests

--- react_intro/task_3/dashborad/src/setupTests.js
+++
@@ -1,8 +0,0 @@
-import "@testing-library/jest-dom";
-import Enzyme from "enzyme";
-import Adapter from "@cfaester/enzyme-adapter-react-18";
-import { TextEncoder, TextDecoder } from "util";
-
-Object.assign(global, { TextDecoder, TextEncoder });
-
-Enzyme.configure({ adapter: new Adapter() });
