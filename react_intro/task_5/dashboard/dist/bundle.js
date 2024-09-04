a7580f3014b7676611b54b76f790bb78d8cd0a77
MariamCHARRADA <charradamariam@yahoo.com>
2024-09-04 12:12:08 +0100

    npm run build done!

--- react_intro/task_5/dashboard/dist/bundle.js
+++ react_intro/task_5/dashboard/dist/bundle.js
@@ -2,7 +2,7 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-console.log("Hello, Webpack with Hot Reloading and Image Loading!");
+console.log("npm run build done!");
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQUEsT0FBTyxDQUFDQyxHQUFHLENBQUMsc0RBQXNELENBQUMsQyIsInNvdXJjZXMiOlsid2VicGFjazovL2Rhc2hib2FyZC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIkhlbGxvLCBXZWJwYWNrIHdpdGggSG90IFJlbG9hZGluZyBhbmQgSW1hZ2UgTG9hZGluZyFcIik7XG4iXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJzb3VyY2VSb290IjoiIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQUEsT0FBTyxDQUFDQyxHQUFHLENBQUMscUJBQXFCLENBQUMsQyIsInNvdXJjZXMiOlsid2VicGFjazovL2Rhc2hib2FyZC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIm5wbSBydW4gYnVpbGQgZG9uZSFcIik7XG4iXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJzb3VyY2VSb290IjoiIn0=
