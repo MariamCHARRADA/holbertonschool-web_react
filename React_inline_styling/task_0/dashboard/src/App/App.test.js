89202f0c214a88e1a21abe0eb1064881ec6e1752
MariamCHARRADA <charradamariam@yahoo.com>
2024-09-20 22:45:58 +0100

    init

---
+++ React_inline_styling/task_0/dashboard/src/App/App.test.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import App from "./App";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import CourseList from "../CourseList/CourseList";
+import Notifications from "../Notifications/Notifications";
+
+describe("<App />", () => {
+  it("renders App component without crashing", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it("renders Notifications component", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Notifications).exists()).toBe(true);
+  });
+
+  it("renders Header component", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Header).exists()).toBe(true);
+  });
+
+  it("renders Footer component", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Footer).exists()).toBe(true);
+  });
+
+  it("renders Login component when isLoggedIn is false", () => {
+    const wrapper = shallow(<App isLoggedIn={false} />);
+    expect(wrapper.find(Login).exists()).toBe(true);
+  });
+
+  it("renders CourseList component when isLoggedIn is true", () => {
+    const wrapper = shallow(<App isLoggedIn={true} />);
+    expect(wrapper.find(CourseList).exists()).toBe(true);
+  });
+
+  it("calls logOut and displays alert when Ctrl+h is pressed", () => {
+    const logOutMock = jest.fn();
+    const alertMock = jest.spyOn(window, "alert").mockImplementation(() => {});
+
+    const wrapper = mount(<App logOut={logOutMock} />);
+
+    const event = new KeyboardEvent("keydown", { ctrlKey: true, key: "h" });
+    window.dispatchEvent(event);
+
+    expect(alertMock).toHaveBeenCalledWith("Logging you out");
+    expect(logOutMock).toHaveBeenCalled();
+
+    alertMock.mockRestore();
+    wrapper.unmount();
+  });
+});
---
+++ React_inline_styling/task_0/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,35 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+
+class NotificationItem extends PureComponent {
+
+  render() {
+    const { type, value, html, markAsRead, id } = this.props;
+
+    const handleClick = () => {
+      markAsRead(id);
+    };
+    
+    return (
+      <li onClick={handleClick} data-notification-type={type}>
+        {html ? <span dangerouslySetInnerHTML={html} /> : value}
+      </li>
+    );
+  }
+}
+
+NotificationItem.propTypes = {
+  type: PropTypes.string.isRequired,
+  html: PropTypes.shape({ __html: PropTypes.string }),
+  value: PropTypes.string,
+  markAsRead: PropTypes.func,
+  id: PropTypes.number,
+};
+
+NotificationItem.defaultProps = {
+  type: "default",
+  html: null,
+  value: "",
+};
+
+export default NotificationItem;
