7f7cfb8f52899e7435df276d025c7ddba5ecb9ca
MariamCHARRADA <charradamariam@yahoo.com>
2024-10-28 20:46:59 +0100

    Create actions for the notification list

---
+++ 0x08_react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionTypes.js
@@ -0,0 +1,7 @@
+export const MARK_AS_READ = "MARK_AS_READ";
+export const SET_TYPE_FILER = "SET_TYPE_FILTER";
+
+export const NotificationTypeFilters = {
+  DEFAULT: "DEFAULT",
+  URGENT: "URGENT",
+};
