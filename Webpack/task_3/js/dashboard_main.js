33a8c6f1196acfeeb12c279b5e84f946e26940b6
MariamCHARRADA <charradamariam@yahoo.com>
2025-02-28 13:31:11 +0100

    logo fix

--- Webpack/task_3/js/dashboard_main.js
+++ Webpack/task_3/js/dashboard_main.js
@@ -1,26 +1,19 @@
-import $ from 'jquery';
-import _ from 'lodash';
-import '../css/main.css';
-import logo from '../assets/holberton-logo.jpg';
+import $ from "jquery";
+import _ from "lodash";
+import "../css/main.css";
 
-$(document).ready(function () {
-  // Add logo element
-  $('body').append('<div id="logo"></div>');
+$("body").append('<div id="logo"></div>');
 
-  // Add elements to the body
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<button>Click here to get started</button>");
+$("body").append('<p id="count"></p>');
+$("body").append("<p>Copyright - Holberton School</p>");
 
-  // Counter function
-  let count = 0;
-  function updateCounter() {
-    count++;
-    $('#count').text(`${count} clicks on the button`);
-  }
+let count = 0;
+function updateCounter() {
+  count++;
+  $("#count").text(`${count} clicks on the button`);
+}
 
-  // Bind debounced function to button click
-  $('button').on('click', _.debounce(updateCounter, 500));
-});
+$("button").on("click", _.debounce(updateCounter, 500));
